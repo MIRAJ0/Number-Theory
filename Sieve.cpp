5c72e5d50b96c4c82cf0ab2101917fbf93e60fb8
MIRAJ0 <112149973+MIRAJ0@users.noreply.github.com>
2024-06-09 14:08:38 +0600

    Update Sieve.cpp

--- Sieve.cpp
+++ Sieve.cpp
@@ -76,6 +76,42 @@ for (int i=2; i <= N; ++i) {
 }
 
 
+
+
+vector<bool>slvarr;     /// part 01  segmentedSieve
+ll siv_H=0;
+void sieve(ll slv) {
+    if (siv_H>slv) return;
+    siv_H=slv;
+    slvarr.assign(slv + 1, false);
+    slvarr[0] = slvarr[1] = 1;
+    for (ll i = 4; i <= slv; i += 2) slvarr[i] = 1;
+    for (ll i = 3; i * i <= slv; i += 2) {
+        if (!slvarr[i]) 
+        for (ll j = i * i; j <= slv; j += 2 * i) 
+        slvarr[j] = 1;
+        }
+}
+void segmentedSieve(ll a, ll b) {    ////  part 2   segmentedSieve
+    ll limit = sqrt(b) + 1;
+    vector <bool>siv_a_b(b-a+1,1);
+    sieve(limit);
+    for(ll i=2;i<limit;i++)
+    if (!slvarr[i])
+    for (ll j = i * i; j <= b; j += i) 
+    if (j>=a)
+    siv_a_b[j-a]=0;
+
+    if (a == 1) siv_a_b[0] = false;
+
+    for (ll i=0;i<siv_a_b.size();i++)
+    if (siv_a_b[i]) cout <<i+a<<endl;
+
+    
+}
+
+
+
 int main ()
 {
 ios_base::sync_with_stdio(0);cin.tie(0);
